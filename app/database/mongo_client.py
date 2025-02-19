9c89e11128b223dd6063069e4febc0827f6bd28c
Juan Camilo Orjuela <juan.orjuela@convatec.com>
2025-02-19 08:57:54 -0500

    feat(SR-30): setup mongoDB database and connection with the project

--- app/database/mongo_client.py
+++ app/database/mongo_client.py
@@ -1,12 +1,11 @@
 from pymongo import MongoClient
-import os
+from app.config.settings import DATABASE_URL
 
-MONGO_URI = os.getenv("DATABASE_URL", "mongodb+srv://juan:P2drmvyxm59Bh1vS@caikei.i4nc3u3.mongodb.net/synastr-dev")
-
-client = MongoClient(MONGO_URI)
+client = MongoClient(DATABASE_URL)
 db = client["synastr-dev"]
 
 users_collection = db["users"]
 messages_collection = db["messages"]
 compatibility_collection = db["compatibility"]
 reports_collection = db["reports"]
+
